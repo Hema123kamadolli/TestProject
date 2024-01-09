c5df4f02c14d2829dbc06e79467d8e00304dfce4
Hema123kamadolli <hema.kamadolli@mphasis.com>
2024-01-09 15:37:50 +0530

    Initial Commit

--- obj/Debug/net6.0/TestProjectOne.AssemblyInfo.cs
+++ obj/Debug/net6.0/TestProjectOne.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProjectOne")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+691b189e69e94be51017444cea6e3c0ffab4fb67")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProjectOne")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProjectOne")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
